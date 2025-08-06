10df0c881c771151137ce61205e6bea0e47a7335
Manish <[manishbulchandani4@gmail.com]>
2025-08-06 17:50:17 +0530

    Industry page spacing increased

--- client/src/components/formSections/FormSection3.tsx
+++ client/src/components/formSections/FormSection3.tsx
@@ -285,7 +285,7 @@ const FormSection3 = ({
           </div>
         </div>
 
-        <div className="w-full h-[62vh] pb-4 mt-3 grid grid-cols-2 gap-2 overflow-y-scroll custom-scrollbar">
+        <div className="w-full h-[62vh] pb-28 mt-3 grid grid-cols-2 gap-2 overflow-y-scroll custom-scrollbar">
           {industries.map((industry, index) => (
             <IndustryBox
               key={index}
