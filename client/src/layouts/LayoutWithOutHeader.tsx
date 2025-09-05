62c8d11ca7e9444ad28f31833d0e47eebe891222
Manish <[manishbulchandani4@gmail.com]>
2025-09-05 15:40:51 +0530

    window resize adaption fix

--- client/src/layouts/LayoutWithOutHeader.tsx
+++ client/src/layouts/LayoutWithOutHeader.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 
 const LayoutWithOutHeader = () => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div
       className="w-full flex flex-col flex-1 bg-whiteBG"
       style={{
-        minHeight: innerHeight,
-        height: innerHeight,
+        minHeight: windowHeight,
+        height: windowHeight,
       }}
     >
       <div className="flex-[0.97] w-full h-full overflow-y-hidden">
